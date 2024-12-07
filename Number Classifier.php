eb1aa3ca9218310973e1ea0324f8c8979cad2aee
Himel <dmjunkie303012@gmail.com>
2024-12-08 01:28:02 +0600

    First one

---
+++ Number Classifier.php
@@ -0,0 +1,17 @@
+<?php
+
+$number = (int)readline("Enter a number: ");
+
+if ($number > 0) {
+    echo "$number is positive.\n";
+    return;
+}
+
+if ($number < 0) {
+    echo "$number is negative.\n";
+    return;
+}
+
+echo "$number is 0.\n";
+
+?>
